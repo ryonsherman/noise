62fd183f11bc686a5d7d3f771877330c5dfdef5e
Ryon Sherman <ryon.sherman@gmail.com>
2014-10-08 13:02:27 -0500

    Added markdown support for template rendering

--- src/noise/template.py
+++ src/noise/template.py
@@ -5,7 +5,9 @@ __email__     = "ryon.sherman@gmail.com"
 __copyright__ = "Copyright 2014, Ryon Sherman"
 __license__   = "MIT"
 
+import os
 import jinja2
+import markdown
 
 from jinja2.exceptions import TemplateNotFound
 
@@ -23,6 +25,23 @@ BOILERPLATE = \
 </html>
 """.strip()
 
+md = markdown.Markdown(extensions=[
+    'toc',
+    'abbr',
+    'tables',
+    'codehilite'
+])
+
+def markdown_filter(text):
+    if os.path.exists(text):
+        with open(text, 'r') as f:
+            data = f.read()
+    return md.convert(text).strip()
+
+def markdown_toc(text):
+    markdown_filter(text)
+    return md.toc.strip()
+
 
 class NoiseTemplateHelper(object):
     def __init__(self, app):
@@ -31,6 +50,10 @@ class NoiseTemplateHelper(object):
         # set app instance in jinja globals
         self.env.globals.update({
             'app': app
+        })
+        # set additional jinja filters
+        self.env.filters.update({
+            'markdown': markdown_filter
         })
 
     def render(self, template, **data):
